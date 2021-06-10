ddb2e0d45a95bd045d811bd6617c83d07da3c9af
Pauline De Ro <pauline.dero@gmail.com>
2021-06-10 13:56:38 +0200

    installed npm node-fetch + create script : getDiameter

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
-// Write you code here
+/* Installed node module 'node-fetch' to get data from the Star Wars API */
+const fetch = require('node-fetch');
+
+/* 
+The use of Async / Await gives me the possibility to write a code easier to read
+(instead of .then()).
+The "try...catch" instruction allows me to alert the user in case of error during the process.
+*/
+
+// Get the argument of the command line instruction
+const id = process.argv[2];
+
+// Provide an error message for bad request
+if (id < 1 || id > 6) {
+  console.log('No Star Wars movie found for this Id. Please provide a number from 1 to 6.')
+
+} else {
+
+  async function getSumOfDiameter(id) {
+    try {
+      // Stock the data in a variable. Makes further treatment easier.
+      let response = await fetch(`https://swapi.dev/api/films/${id}`);
+
+      // Transform the data in JSON-format. Stock the JSON-object in a variable called "movieData"
+      let movieData = await response.json();
+
+      // Decide to stock only useful property with "array destructuring".
+      let { planets } = movieData;
+
+      // Create a variable to stock all the future results of my filter. 
+      let result = [];
+
+      // The "for loop" allows to fetch all the planets of a movie and apply data treatment.
+      for (let i = 0; i < planets.length; i++) {
+        response = await fetch(planets[i]);
+        let planetData = await response.json();
+        let { terrain, surface_water, diameter } = planetData;
+
+        // Push in "result" the diameter of the planets that correspond to my conditions
+        terrain.includes('mountains') && surface_water != 'unknown' && surface_water != '0'
+          ? result.push(parseInt(diameter))
+          : null;
+      };
+
+      // Use of reduce method to get the sum of all diameters
+      console.log(result.reduce((a, b) => a + b, 0));
+
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  getSumOfDiameter(id);
+};
+
